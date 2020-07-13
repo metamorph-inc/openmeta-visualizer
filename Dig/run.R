d93b45ef27b7707541a4555cc825e50799e4e16f
Jonathon Williams <jwilliams@metamorphsoftware.com>
2020-07-13 17:37:48 -0500

    Add OPENMETA_VISUALIZER_OPEN_IN_BROWSER environment variable to launch in default browser instead of Electron app (value doesn't matter, only that it is or isn't set)

--- Dig/run.R
+++ Dig/run.R
@@ -1,6 +1,13 @@
-# shiny::runApp('Dig',display.mode='normal',quiet=FALSE, launch.browser=TRUE)
+# 
 
-shiny::runApp('Dig',display.mode='normal',quiet=FALSE, launch.browser=function(url) {
-    electronApp = file.path("..", "viz_electron_bin", "viz-electron.exe", fsep="\\")
-    system2("cmd.exe", c("/c", electronApp, url), wait=FALSE)
-})
+useBrowserVar <- Sys.getenv("OPENMETA_VISUALIZER_OPEN_IN_BROWSER", unset=NA)
+
+if(is.na(useBrowserVar)) {
+    shiny::runApp('Dig',display.mode='normal',quiet=FALSE, launch.browser=function(url) {
+        electronApp = file.path("..", "viz_electron_bin", "viz-electron.exe", fsep="\\")
+        system2("cmd.exe", c("/c", electronApp, url), wait=FALSE)
+    })
+} else {
+    shiny::runApp('Dig',display.mode='normal',quiet=FALSE, launch.browser=TRUE)
+}
+
